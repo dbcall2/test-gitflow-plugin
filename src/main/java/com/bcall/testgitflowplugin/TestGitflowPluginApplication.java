28c1a7fd11b4f097cd8ae9153f98c787c809690d
briancall <briancall@192.168.1.12>
2018-03-06 10:25:11 -0800

    Initial commit.

---
+++ src/main/java/com/bcall/testgitflowplugin/TestGitflowPluginApplication.java
@@ -0,0 +1,12 @@
+package com.bcall.testgitflowplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitflowPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitflowPluginApplication.class, args);
+	}
+}
